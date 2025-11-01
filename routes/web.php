80d6fcaddb688e01c667af51c86448f32f53b4cb
Muqri Amin Bin Mohd Shamsuddin <muqri.amin@artrustees.com.my>
2025-11-02 00:54:16 +0800

    refactor(routes): remove duplicate stationary report route

--- routes/web.php
+++ routes/web.php
@@ -191,7 +191,6 @@ Route::group([
     Route::get('/reports/user-activity-report', [ReportController::class, 'userActivityReport'])->name('reports.user-activity');
     Route::get('/reports/stationary-report', [ReportController::class, 'stationaryReport'])->name('reports.stationary');
     Route::get('/reports/dashboard-report', [ReportController::class, 'dashboardReport'])->name('reports.dashboard');
-    Route::get('/reports/stationary', [ReportController::class, 'stationaryReport'])->name('reports.stationary');
 
     Route::get('/cron', [CronJobController::class, 'index'])->name('cron.index');
     Route::post('/cron/run-command', [CronJobController::class, 'runCommand'])->name('cron.run-command');
